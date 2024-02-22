f05cf3e19d87f57ecffb90d0778cf25aad667f90
Neoul <55011525+neoul-waves@users.noreply.github.com>
2024-02-22 12:15:35 +0900

    chore: update dependencies and navigation setup

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import {ApplicationProvider, IconRegistry, Layout, Text} from '@ui-kitten/components';
+import {ApplicationProvider, IconRegistry} from '@ui-kitten/components';
 import {BottomNavigationAccessoriesShowcase} from './components/bottomNav';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import {EvaIconsPack} from '@ui-kitten/eva-icons';
 import Home from './pages/Home';
+import {NavigationContainer} from '@react-navigation/native';
 
 export default () => (
-  <>
+  <NavigationContainer>
     <IconRegistry icons={EvaIconsPack} />
     <ApplicationProvider {...eva} theme={eva.light}>
       <Home />
       <BottomNavigationAccessoriesShowcase />
     </ApplicationProvider>
-  </>
+  </NavigationContainer>
 );
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import {ApplicationProvider, IconRegistry, Layout, Text} from '@ui-kitten/components';
-import {BottomNavigationAccessoriesShowcase} from './components/bottomNav';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import * as React from 'react';
+import {Layout, Text} from '@ui-kitten/components';
 
 export default () => (
   <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
