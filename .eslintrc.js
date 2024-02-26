eeda416ae110ed3f5cb002868a8a1217c94177e4
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-02-26 17:58:05 +0900

    chore: update ESLint and Prettier configurations

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react-native/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    '@react-native',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
