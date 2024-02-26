2438f9038406abd7c602dfd7b99a87a06bf422be
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-02-26 20:07:01 +0900

    fix: dependency proble

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import {createStackNavigator} from '@react-navigation/stack'
 
 import { HomeScreen } from './home'
-
 import {AppRoute} from './config'
 
 const Stack = createStackNavigator()
 
 export const AppNavigator = (props: any): React.ReactElement => (
-  <Stack.Navigator {...props} headerMode="none">
+  <Stack.Navigator {...props} screenOptions={false}>
     <Stack.Screen name={AppRoute.Home} component={HomeScreen} />
   </Stack.Navigator>
 )
